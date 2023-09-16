8018d95e8437046c9fc210a818e6738a5ead9a58
kbinhnguyen <kbinhnguyen.nihongo@gmail.com>
2023-09-16 17:24:54 -0400

    app navigator

--- nurse_app/AppNavigator.js
+++ nurse_app/AppNavigator.js
@@ -3,23 +3,26 @@ import { createStackNavigator } from '@react-navigation/stack';
 import IntroVideoScreen from './screens/IntroVideoScreen';
 import LoginScreen from './screens/LoginScreen';
 import SignupScreen from './screens/SignupScreen';
-import TestPage from './screens/TestPage';
-
 import HomeScreen from './screens/HomeScreen';
+import StartDrip from './screens/StartDrip';
+import Titrate from './screens/Titrate';
+
 
 const Stack = createStackNavigator();
 
 function AppNavigator() {
   return (
     <Stack.Navigator
-      initialRouteName="Login"
+      // initialRouteName="Login"
       screenOptions={{  headerShown: false}}
     >
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="Signup" component={SignupScreen} />
       <Stack.Screen name="IntroVideo" component={IntroVideoScreen} />
-      <Stack.Screen name="Test" component={TestPage} />
       <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="StartDrip" component={StartDrip} />
+      <Stack.Screen name="Titrate" component={Titrate} />
+
     </Stack.Navigator>
   );
 }
