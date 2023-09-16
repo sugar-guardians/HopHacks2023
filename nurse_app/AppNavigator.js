5fc190a02aa4149dab7ac46bc17ce32507d434b4
kbinhnguyen <kbinhnguyen.nihongo@gmail.com>
2023-09-16 17:26:43 -0400

    resolve merge conflicts

--- nurse_app/AppNavigator.js
+++ nurse_app/AppNavigator.js
@@ -13,13 +13,13 @@ const Stack = createStackNavigator();
 function AppNavigator() {
   return (
     <Stack.Navigator
-      initialRouteName="Login"
+      // initialRouteName="Login"
       screenOptions={{  headerShown: false}}
     >
-      <Stack.Screen name="Login" component={LoginScreen} />
+      {/* <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="Signup" component={SignupScreen} />
       <Stack.Screen name="IntroVideo" component={IntroVideoScreen} />
-      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Home" component={HomeScreen} /> */}
       <Stack.Screen name="StartDrip" component={StartDrip} />
       <Stack.Screen name="Titrate" component={Titrate} />
 
