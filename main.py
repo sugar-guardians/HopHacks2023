8207136d5476a16c472928b2185d6ed84235d064
TirthOfficials <tirthppatelofficial@gmail.com>
2023-09-17 00:03:35 -0400

    twilio

--- main.py
+++ main.py
@@ -4,6 +4,21 @@ from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
 from bson import ObjectId
+from fastapi import BackgroundTasks
+from twilio.rest import Client
+
+account_sid = 'AC6380f4e379a140460b4b532bfa68ed94'
+auth_token = 'ecb058ec180dd0e1eb54a56ccf9fcab6'
+client = Client(account_sid, auth_token)
+def send_sms(phone_number):
+    for i in range(5):
+        message = client.messages.create(
+          from_='+18555142486',
+          body='Reminder!!!\nTime to check BG and Titrate.',
+          to=phone_number
+        )
+        print(message.sid)
+        # time.sleep(12)  # Wait for 12 seconds before sending the next SMS
 
 def nurse_to_json(nurse):
     nurse['id'] = str(nurse['_id'])
@@ -33,17 +48,20 @@ class NurseLogin(BaseModel):
     password: str
 
 @app.post("/signup/")
-async def create_nurse(nurse: NurseBase):
+async def create_nurse(background_tasks: BackgroundTasks, nurse: NurseBase):
     if db.nurses.find_one({"nurseID": nurse.nurseID}):
         raise HTTPException(status_code=400, detail="NurseID already registered")
     db.nurses.insert_one(nurse.dict())
     nurse_data1 = db.nurses.find_one({"nurseID": nurse.nurseID})
+    background_tasks.add_task(send_sms, nurse.phone)
     return {"message": "Nurse created successfully"}
 
 
 @app.post("/login/")
-async def login_nurse(nurse: NurseLogin):
+async def login_nurse(background_tasks: BackgroundTasks, nurse: NurseLogin):
     nurse_data = db.nurses.find_one({"nurseID": nurse.nurseID, "password": nurse.password})
     if nurse_data:
+        background_tasks.add_task(send_sms, nurse.phone)
         return {"message": "Login successful"}
+
     raise HTTPException(status_code=400, detail="Invalid credentials")
